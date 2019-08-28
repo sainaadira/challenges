1c52b23a090e7835bd39d443e20d611d50cb378e
Sai <snaadira@gmail.com>
2019-08-28 14:34:47 -0400

    completed codewars

---
+++ codewars/js/8kyu/evenOrOdd/main.js
@@ -0,0 +1,14 @@
+
+// https://www.codewars.com/kata/even-or-odd/train/javascript
+
+// Create a function (or write a script in Shell) that takes 
+// an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. 
+
+function even_or_odd(number) {
+    if (number % 2 === 0) {
+        return 'Even'
+    } else {
+        return 'Odd'
+    }
+}
+console.log(even_or_odd(14))
