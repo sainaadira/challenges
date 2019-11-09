6314e7a8e2586716716b42e5ae461d1686fa231a
Sai <snaadira@gmail.com>
2019-11-09 11:22:45 -0500

    completed challenge

---
+++ codewars/js/8kyu/convertStrToNum/main.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/convert-a-string-to-a-number/train/javascript
+
+/* We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+Examples
+stringToNumber("1234") == 1234
+stringToNumber("605" ) == 605
+stringToNumber("1405") == 1405
+stringToNumber("-7"  ) == -7 */
+
+var stringToNumber = function(str){
+   const count = parseInt(str)
+  return count
+}
