359d1fe2f518653338e94270046c0f470fb13be7
Sai <snaadira@gmail.com>
2019-11-17 13:00:30 -0500

    completed challenge

---
+++ codewars/js/8kyu/makeUpperCase/main.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/makeuppercase/train/javascript
+
+// Write function makeUpperCase.
+
+function makeUpperCase(str){
+  return str.toUpperCase()
+}
+makeUpperCase('javascript is fun')
