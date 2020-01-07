6b7d00ff34079cce8bdafb1b832b6c7200673a9d
Sai <snaadira@gmail.com>
2020-01-07 11:57:08 -0500

    completed challenge

---
+++ codewars/js/8kyu/getCharFromASCIIVal/main.js
@@ -0,0 +1,12 @@
+/* https://www.codewars.com/kata/get-character-from-ascii-value/train/javascript
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+get_char(65) # => 'A' */
+
+
+
+function getChar(c){
+  return String.fromCharCode(c)
+}
