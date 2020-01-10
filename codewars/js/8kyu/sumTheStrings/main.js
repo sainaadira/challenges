1f3f855eb952f619a298d7baad1ba13365b7d522
Sai <snaadira@gmail.com>
2020-01-10 12:08:31 -0500

    completed challenge

---
+++ codewars/js/8kyu/sumTheStrings/main.js
@@ -0,0 +1,12 @@
+/*https://www.codewars.com/kata/5966e33c4e686b508700002d/train/javascript
+
+Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
+
+  sumStr("4", "5")    // should output "9"
+  sumStr("34", "5")   // should output "39" */
+
+
+  function sumStr(a,b) {
+    
+  return  `${Number(a) + Number(b)}`
+}
