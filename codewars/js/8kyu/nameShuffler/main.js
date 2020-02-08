da24e4bfd7200255d24526efaf3061a7fa299216
Sai <snaadira@gmail.com>
2020-02-08 09:16:42 -0500

    completed solution

---
+++ codewars/js/8kyu/nameShuffler/main.js
@@ -0,0 +1,10 @@
+/*
+https://www.codewars.com/kata/559ac78160f0be07c200005a/train/javascript
+
+Write a function that returns a string in which firstname is swapped with last name.
+nameShuffler('john McClane'); => "McClane john" */
+
+
+function nameShuffler(str){
+  return str.split(' ').reverse().join(' ')
+}
