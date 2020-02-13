625e1cfbff50b365d4103cbfff90b59a8338eb29
Sai <snaadira@gmail.com>
2020-02-12 23:33:17 -0500

    completed solution

---
+++ codewars/js/8kyu/beginnerSeries-SchoolPaperwork/main.js
@@ -0,0 +1,19 @@
+/* https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/train/javascript
+
+Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+Your task is to calculate how many blank pages do you need.
+
+Example:
+paperwork(5, 5) == 25
+Note! if n or m < 0 return 0! Waiting for translations and Feedback! Thanks!
+
+*/
+
+
+function paperwork(n, m) {
+  if (m < 0 || n < 0) {
+    return 0;
+  }
+  return m * n;
+}
