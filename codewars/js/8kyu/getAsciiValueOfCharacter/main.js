d47924df181385a56301cb978fd1178ffa92ee8d
Sai <snaadira@gmail.com>
2020-03-15 08:20:35 -0400

    completed solution

---
+++ codewars/js/8kyu/getAsciiValueOfCharacter/main.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/55acfc59c3c23d230f00006d/train/javascript
+
+get ascii value of character
+
+*/
+
+
+function getASCII(c){
+  let value = c.charCodeAt()
+  return value
+}
