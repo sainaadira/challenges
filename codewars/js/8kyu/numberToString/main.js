349ee736a2e9196fbe6085c99618b15bbf786798
Sai <snaadira@gmail.com>
2020-03-30 14:01:28 -0400

    completed solution

---
+++ codewars/js/8kyu/numberToString/main.js
@@ -0,0 +1,16 @@
+
+/*
+https://www.codewars.com/kata/53934feec44762736c00044b/train/javascript
+
+The code gives an error!
+
+a = 123.toString
+Fix it!
+
+var a = 123.toString(); // this code gives an error
+
+*/
+
+
+let number = 123
+let a = number.toString()
