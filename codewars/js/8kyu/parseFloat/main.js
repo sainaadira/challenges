d16912f48987711f6691417f216068e1ac9e49d0
Sai <snaadira@gmail.com>
2020-08-17 17:00:45 -0400

    completed solution

---
+++ codewars/js/8kyu/parseFloat/main.js
@@ -0,0 +1,13 @@
+/* https://www.codewars.com/kata/57a386117cb1f31890000039/train/javascript
+
+Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware. */
+
+
+function parseF(s) {
+  if (parseFloat(s) === 0) {
+    return 0;
+  }
+  else {
+    return parseFloat(s) || null
+  }
+  }
