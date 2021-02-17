66d013c81f225f11a7b30eb9b9e8f63ed2549ae5
Sai <snaadira@gmail.com>
2021-02-17 06:48:07 -0500

    completed vowelsCounter

--- algo-challenge/Beginner/vowelsCounter/index-START.js
+++ algo-challenge/Beginner/vowelsCounter/index-START.js
@@ -4,10 +4,20 @@ e.g vowelsCounter('anehizxcv') // will return 3
 */
 
 
-function vowelsCounter(text) {
-    // Code goes here
+
+const vowelsCounter = (text) => {
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let count = 0
+
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] === 'a' || text[i] === 'e' || text[i] === 'i' || text[i] === 'o' || text[i] === 'u') {
+            count++
+        }
+    }
+    return count
 }
 
-
+console.log(vowelsCounter('test argument')
+)
 
 module.exports = vowelsCounter;
