180aa9f0f9c64179349dfcc00ef67403496c3c9b
Sai <snaadira@gmail.com>
2021-02-17 17:35:57 -0500

    completed chunkArr solution

--- algo-challenge/Beginner/chunkArray/index-START.js
+++ algo-challenge/Beginner/chunkArray/index-START.js
@@ -15,17 +15,4 @@ const chunkArray = (array, size) => {
 console.log(chunkArray([1, 2, 3, 4], [5, 6, 1, 2, 3]))
 
 
-
-// this way worked but i didnt use any of the parameters
-// function chunkArray(array, size) {
-//     let arr1 = [1, 2, 3, 4, 5]
-//     let arr2 = [5, 6, 7, 2]
-//     let arr3 = [...arr1, ...arr2]
-//     let filteredArr = arr3.filter((index, item) => arr3.indexOf(index) === item)
-//     return filteredArr
-// }
-// console.log(chunkArray())
-
-
-
 module.exports = chunkArray
