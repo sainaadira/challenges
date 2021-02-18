64d56e68ad2ea990258aeb50980da7b2c4005206
Sai <snaadira@gmail.com>
2021-02-18 18:54:59 -0500

    completed solution with comments

--- algo-challenge/Beginner/whereIBelong/index-START.js
+++ algo-challenge/Beginner/whereIBelong/index-START.js
@@ -7,12 +7,17 @@ whereIBelong([1,2,3,4], 1.5) // should return 1 because it is greater
 than 1(index 0), but less than 2(index 1).
 */
 
+//data working with: array => returns a number
 
-
-function whereIBelong(arr, num) {
-   // Code goes here
-
+const whereDoIBelong = (arr, num) => {
+   // pushing the number into the array
+   arr.push(num)
+   // sort through the array in ascending order
+   arr.sort((a, b) => a - b)
+   // return the index of the newly pushed number
+   return arr.indexOf(num)
 }
+console.log(whereDoIBelong([1, 2, 5, 6], 3))
 
 
 
