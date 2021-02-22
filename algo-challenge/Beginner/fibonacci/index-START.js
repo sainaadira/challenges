7c1d544fa5915e13ad887800a6060d1607e75e65
Sai <snaadira@gmail.com>
2021-02-22 16:45:48 -0500

    completed fibonacci solution

--- algo-challenge/Beginner/fibonacci/index-START.js
+++ algo-challenge/Beginner/fibonacci/index-START.js
@@ -4,9 +4,23 @@ where the sequence is:
     [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, …]
 */
 
+// data working with: number
+// create variable to store first and second value
+// variable to store the current val to the previous one
+// loop through the numbers while n is greater than/equal to 0
 
-function fibonacci(n) {
-    // Code goes here
+
+const fibonacci = (n) => {
+    let firstVal = 1, secondVal = 2, tempSum;
+    while (n >= 0) {
+        tempSum = firstVal
+        firstVal = firstVal + secondVal
+        secondVal = tempSum
+        n--
+    }
+    return secondVal
 }
+
+fibonacci(4)
 
 module.exports = fibonacci
