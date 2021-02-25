1baf8f49e2d48aba425187d70d3b037b0690f188
Sai <snaadira@gmail.com>
2021-02-25 07:35:02 -0500

    completed fizzBuzz solution

--- algo-challenge/Beginner/fizzBuzz/index-START.js
+++ algo-challenge/Beginner/fizzBuzz/index-START.js
@@ -7,9 +7,20 @@
 
 
 
-function fizzBuzz(n) {
-    // Code goes here
+const fizzBuzz = (n) => {
+    for (let n = 1; n <= 100; n++) {
+        if (n % 15 === 0) {
+            console.log('FizzBuzz')
+        } else if (n % 3 === 0) {
+            console.log('Fizz')
+        } else if (n % 5 === 0) {
+            console.log('Buzz')
+        } else {
+            console.log(n)
+        }
+    }
 }
 
+fizzBuzz()
 
 module.exports = fizzBuzz
