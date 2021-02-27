42fbec890e6fe26cf197ddc9b5ded7b41b868653
Sai <snaadira@gmail.com>
2021-02-27 08:33:48 -0500

    completed factorial solution

--- algo-challenge/Beginner/factorial/index-START.js
+++ algo-challenge/Beginner/factorial/index-START.js
@@ -4,16 +4,17 @@ the provided integer(n). E.g
     factorial(5) // should return 120
 */
 
-
-function factorial(n, memo) {
+const factorial = (n, memo) => {
     memo = memo || {}
-    if(memo[n]){
+    if (memo[n]) {
         return memo[n]
-    }  
+    }
     if (n === 0) {
         return 1
     }
     return memo[n] = n * factorial(n - 1, memo)
 }
+
+console.log(factorial(5))
 
 module.exports = factorial
