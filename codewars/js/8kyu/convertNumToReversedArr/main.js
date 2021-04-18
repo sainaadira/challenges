9258c5e7707e04ea2520725653aac93bbdfdac95
Sai <snaadira@gmail.com>
2021-04-17 20:54:16 -0400

    completed convertNum solution

---
+++ codewars/js/8kyu/convertNumToReversedArr/main.js
@@ -0,0 +1,26 @@
+/* https://www.codewars.com/kata/5583090cbe83f4fd8c000051/train/javascript
+
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+Example:
+348597 => [7,9,5,8,4,3]
+
+
+data working with: numbers + arrays
+returns: array
+
+to split and reverse: turn x into a string
+
+*/
+
+const digitize = n => {
+  n = `${n}.`.split('').reverse()
+  let newArr = []
+
+  for (let i = 0; i < x.length; i++) {
+    newArr.push(+ n[i])
+  }
+  return newArr
+
+}
+
