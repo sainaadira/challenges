dfefc356d838b410b37fa543bf11fe171a7bd215
Sai <snaadira@gmail.com>
2021-04-20 18:16:33 -0400

    completed solution

---
+++ codewars/js/8kyu/iLoveYouALittle/main.js
@@ -0,0 +1,31 @@
+/*
+https://www.codewars.com/kata/57f24e6a18e9fad8eb000296/train/javascript
+
+Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+I love you
+a little
+a lot
+passionately
+madly
+not at all
+When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+
+data working with: string
+returns:string
+*/
+
+const howMuchILoveYou = nbPetals => {
+  let lovePhrases = [
+    'I love you',
+    'a little',
+    'alot',
+    'passionately',
+    'madly',
+    'not at all'
+  ]
+  return lovePhrases[(nbPetals - 1)] % 6
+
+}
