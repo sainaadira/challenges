03c7d127b5ac775ac2fc34309f1c71c07803f04f
Sai <snaadira@gmail.com>
2021-04-22 08:23:50 -0400

    updated solution

--- codewars/js/8kyu/welcome/main.js
+++ codewars/js/8kyu/welcome/main.js
@@ -27,27 +27,28 @@ welsh: 'Croeso'
 */
 
 
-function greet(language) {
+const greet = language => {
 
-const greeting = {
-english: 'Welcome',
-czech: 'Vitejte',
-danish: 'Velkomst',
-dutch: 'Welkom',
-estonian: 'Tere tulemast',
-finnish: 'Tervetuloa',
-flemish: 'Welgekomen',
-french: 'Bienvenue',
-german: 'Willkommen',
-irish: 'Failte',
-italian: 'Benvenuto',
-latvian: 'Gaidits',
-lithuanian: 'Laukiamas',
-polish: 'Witamy',
-spanish: 'Bienvenido',
-swedish: 'Valkommen',
-welsh: 'Croeso'
+  const greeting = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  }
+  return greeting[language] || greeting.english
 
 }
- return greeting[language] || greeting.english
-}
+
