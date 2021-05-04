e595edc3379a7771702dacc4b4569267327ff039
Sai <snaadira@gmail.com>
2021-05-04 08:41:25 -0400

    completed solution with comments

---
+++ codewars/js/8kyu/takeAnArrow/main.js
@@ -0,0 +1,19 @@
+/*
+https://www.codewars.com/kata/559f3123e66a7204f000009f/train/javascript
+
+Arrow style Functions
+Come here to practice the Arrow style functions Not much else to say good luck!
+Details
+You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
+
+Examples
+These are example of how to convert a number to an ascii Character:
+Javascript => String.fromCharCode(97) // a
+C# => Tools.FromCharCode(97) // a
+ */
+
+const ArrowFunc = arr => {
+  //map through arr of numbers and convert to a string
+  // fromCharCode() conversts number to an ascii character
+  return arr.map(el => String.fromCharCode(el)).join('');
+}
