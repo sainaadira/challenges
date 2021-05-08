7014c431d72c1ba8b6cd4353230e493c5ec49b5f
Sai <snaadira@gmail.com>
2021-05-08 07:28:17 -0400

    completed solution

---
+++ codewars/js/7kyu/sortArrays/main.js
@@ -0,0 +1,12 @@
+/*
+https://www.codewars.com/kata/51f41b98e8f176e70d0002a8/train/javascript
+
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list in a sorted manner.
+
+
+ */
+
+
+const sortme = names => {
+  return names.sort()
+}
