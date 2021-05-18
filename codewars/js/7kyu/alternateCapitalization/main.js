6284d20c939841403d2ad0557fc8734b9e41be19
Sai <snaadira@gmail.com>
2021-05-18 08:07:23 -0400

    added comments to solution

--- codewars/js/7kyu/alternateCapitalization/main.js
+++ codewars/js/7kyu/alternateCapitalization/main.js
@@ -6,25 +6,49 @@ For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for mor
 
 The input will be a lowercase string with no spaces.
 
-Good luck! */
+Good luck! 
 
+
+*/
 
 function capitalize(s) { //declaring function capitalize expecting one argument
-    let str = ''; //declaring str as an empty string
-    let altString = ''; //declaring altString as an empty string
+    let str = '' //declaring str as an empty string
+    let altString = '' //declaring altString as an empty string
     for (let i = 0; i < s.length; i++) { //for loop iterating over length of argument
         if (i % 2 === 0) { //conditional executing if 0 or an even index
-            str += s[i].toUpperCase(); //transforming letter at current index to upper case and adds to str
+            str += s[i].toUpperCase() //transforming letter at current index to upper case and adds to str
         } else { //conditional executing if other than an even index
-            str += s[i]; //adds current index to str
+            str += s[i] //adds current index to str
         }
     } //end of for loop
     for (let i = 0; i < s.length; i++) { //for loop iterating over length of argument
         if (i % 2 === 1) { //conditonal executing if current index is odd
-            altString += s[i].toUpperCase(); //transforms current index to upper case and adds to altString
+            altString += s[i].toUpperCase() //transforms current index to upper case and adds to altString
         } else { //conditional executing if other than odd index
-            altString += s[i]; //adds current index to altString
+            altString += s[i] //adds current index to altString
         }
     } //end of for loop
-    return [str, altString]; //returns an array with str and altString
+    return [str, altString] //returns an array with str and altString
 };
+
+// without comments
+
+/* const capitalize = s => {
+    let str = ''
+    let altString = ''
+
+    for (let i = 0; i < s.length; i++) {
+        if (i % 2 === 0) {
+            str += str[i].toUpperCase()
+        }
+    }
+    for (let i = 0 i < s.length; i++) {
+        if (i % 2 === 1) {
+            altString += str[i].toUpperCase()
+        } else {
+            altString += s[i]
+        }
+    }
+    return [str, altString]
+}
+*/
