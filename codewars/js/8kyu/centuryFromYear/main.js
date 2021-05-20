c69b2f333d232e7c3dc16c44e527884f933c5301
Sai <snaadira@gmail.com>
2021-05-20 16:55:19 -0400

    updated code with second solution

--- codewars/js/8kyu/centuryFromYear/main.js
+++ codewars/js/8kyu/centuryFromYear/main.js
@@ -15,19 +15,19 @@ centuryFromYear(1601)  returns (17)
 centuryFromYear(2000)  returns (20) */
 
 
-function century(year) {
+const century = year => {
   let century = 0;
 
-  for(let i = 0; i < year; i++) {
-    if(i % 100 === 0) {
+  for (let i = 0; i < year; i++) {
+    if (i % 100 === 0) {
       century++;
     }
   }
   return century;
 }
 
- OR
+OR
 
-function century(year) {
-  return Math.ceil(year/100); // math.ceil to round up to nearest century (100)
+const century = year => {
+  return Math.ceil(year / 100); // math.ceil to round up to nearest century (100)
 }
