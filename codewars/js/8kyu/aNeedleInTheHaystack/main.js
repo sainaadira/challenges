13cf79dc8251701259e0312a4a450a8ce5ac60db
Sai <snaadira@gmail.com>
2021-05-19 20:04:13 -0400

    updated code and added comments

--- codewars/js/8kyu/aNeedleInTheHaystack/main.js
+++ codewars/js/8kyu/aNeedleInTheHaystack/main.js
@@ -9,24 +9,24 @@ After your function finds the needle it should return a message (as a string) th
 "found the needle at position " plus the index it found the needle, so:
 
 findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
-should return "found the needle at position 5" */
-
-// The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present. (mdn)
-
-// 1) function loops through the haystack array searching for the string 'needle' and returning the index
-// 2) console.log a string'found the needle at position' + the returned index
+should return "found the needle at position 5"
 
 
-function findNeedle(haystack) {
-  let needleIndex = haystack.indexOf('needle');
-  return 'found the needle at position ' + needleIndex;
-}
-
-findNeedle(['giraffe', 'dog', 'london', 'needle', 'hello', 'sunflower']);
-
-OR
+data working with: array
+returns: string with index postion of needle
 
 
-function findNeedle(haystack) {
-  return "found the needle at position " + haystack.indexOf("needle");
+
+__pseudocode__
+turn the word needle into a string
+use indexOf() method to find postion of needle on the haystack array
+return a string with 'found the needle at postion with the number of the postion
+
+The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present. (mdn)
+*/
+
+
+const findNeedle = haystack => {
+  let indexPostion = haystack.indexOf('needle')
+  return `found the needle at position ${indexPostion}`
 }
