f7deecc483331f71e8ef69260ca17b1c88ac2f85
Sai <snaadira@gmail.com>
2021-05-19 20:06:04 -0400

    update code and added comments

--- codewars/js/8kyu/areYouPlayingBanjo?/main.js
+++ codewars/js/8kyu/areYouPlayingBanjo?/main.js
@@ -7,10 +7,19 @@ The function takes a name as its only argument, and returns one of the following
 
 name + " plays banjo"
 name + " does not play banjo"
-Names given are always valid strings.*/
+Names given are always valid strings.
+
+data working with: string parameter
+return: string
 
 
-
+___pseudocode___
+take the name param and search if the first letter contains the letter "r"
+make name lowercase so all inputs are cased the same
+charAt()- returns a new string at a specified index of the string
+if name begins with ar return 'r name' plays banjo
+else return name does not play banjo
+*/
 
 const areYouPlayingBanjo = name => {
   if (name.toLowerCase().charAt(0) === 'r') {
