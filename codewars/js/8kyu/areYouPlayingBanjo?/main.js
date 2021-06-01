6a58de28ec80377a8b3784a4825ba9dc5218414d
Sai <snaadira@gmail.com>
2021-05-31 21:54:30 -0400

    added cleaner pseudocode

--- codewars/js/8kyu/areYouPlayingBanjo?/main.js
+++ codewars/js/8kyu/areYouPlayingBanjo?/main.js
@@ -5,13 +5,15 @@ If your name starts with the letter "R" or lower case "r", you are playing banjo
 
 The function takes a name as its only argument, and returns one of the following strings:
 
+
+
+P: data working with: string parameter
+R: return: string
+E: 
 name + " plays banjo"
 name + " does not play banjo"
 Names given are always valid strings.
-
-data working with: string parameter
-return: string
-
+P:
 
 ___pseudocode___
 take the name param and search if the first letter contains the letter "r"
