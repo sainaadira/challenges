a630fe8a3fb71ffff529f1f15f7f177a2c332278
Sai <snaadira@gmail.com>
2021-06-04 11:15:52 -0400

    refactored this solution

--- codewars/js/7kyu/alternateCapitalization/main.js
+++ codewars/js/7kyu/alternateCapitalization/main.js
@@ -1,37 +1,39 @@
-/*https://www.codewars.com/kata/59cfc000aeb2844d16000075/train/javascript
+/*
+https://www.codewars.com/kata/59cfc000aeb2844d16000075/train/javascript
 
 Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
 
 For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
 
-The input will be a lowercase string with no spaces.
+The input will be a lowercase string with no spaces
 
-Good luck! 
-
-data working with: string, array
-param: string
-returns => array
-
-
-
+P: string
+R: array of strings (with even index capital)
+E: 'abcd' => ['AbCd' , 'aBcD']
+P: split my string param into an array
+   create variable to hold both string values (even + odd)
+   traverse though string with for loop 
+   conditionally check if index are even: i % 2 ===0
+   if they are, uppercase even index, leave odd lowercase vice versa
+   return result 
+   
+   edge cases:
+   non-empty str 
 */
 
-
-// refactored code with one for loop
-const capitalize = s => {
-  const arr = s.split('')
-  let str = ''
-  let altStr = ''
+function capitalize(s) {
+  let arr = s.split('')
+  let evenStr = ''
+  let oddStr = ''
 
   for (let i = 0; i < arr.length; i++) {
-    // checks to see if the index is even and if so make the altStr uppercase and leave the first string index as is.
-    if (i % 2) {
-      str += arr[i]
-      altStr += arr[i].toUpperCase()
+    if (i % 2 === 0) {
+      evenStr += arr[i].toUpperCase()
+      oddStr += arr[i]
     } else {
-      str += arr[i].toUpperCase()
-      altStr += arr[i]
+      evenStr += arr[i]
+      oddStr += arr[i].toUpperCase()
     }
   }
-  return [str, altStr]
+  return [evenStr, oddStr]
 }
