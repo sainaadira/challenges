e8ac0459e825e2ea66ec32c10906dadbd137200a
Sai <snaadira@gmail.com>
2021-06-15 08:27:54 -0400

    completed solution with prep

---
+++ codewars/js/7kyu/alternateCase/main.js
@@ -0,0 +1,22 @@
+/*https://www.codewars.com/kata/57a62154cf1fa5b25200031e/train/javascript
+
+Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+
+P: string
+R: string with cases changed
+E: 'hELLO wORLD' => 'Hello World'
+P: split string into an array
+   iterate though string 
+   conditionally check if 
+   letter is lowercase => uppercase
+   letter is uppercase => lowercase
+   maybe iterate with map() to return a new array
+  join string together
+     
+ */
+
+
+const alternateCase = s => {
+  let word = s.split('')
+  return word.map(letter => letter === letter.toUpperCase() ? letter.toLowerCase() : letter.toUpperCase()).join('')
+}
