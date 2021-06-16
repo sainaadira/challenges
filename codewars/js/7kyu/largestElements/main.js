03aea1f945e9bcaa09edfb312ae53533edf21a6a
Sai <snaadira@gmail.com>
2021-06-16 06:42:31 -0400

    modified parameter

---
+++ codewars/js/7kyu/largestElements/main.js
@@ -0,0 +1,23 @@
+/* https://www.codewars.com/kata/53d32bea2f2a21f666000256/train/javascript
+Write a program that outputs the top n elements from a list.
+
+Example:
+
+largest(2, [7,6,5,4,3,2,1])
+// => [6,7]
+
+P: number, array
+R: array containing largest elements from given input number
+E:  largest(2, [7,6,5,4,3,2,1]) => [6,7]
+    largest(3, [11, 12, 13, 1 ]) => [11, 12, 13]
+P: i dont know the input number
+   sort through the array in decending order
+   extract the largest elements based on given input number .. slice()?
+   because it will extract from 0th index to the given input number
+   reverse all elements in array
+
+*/
+
+const largest = (n, arr) => {
+  return arr.sort((b, a) => a - b).slice(0, n).reverse()
+}
