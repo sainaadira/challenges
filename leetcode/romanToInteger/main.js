2eb650a59420b58de149e3f5b43bba931475659e
Sai <snaadira@gmail.com>
2021-06-30 12:53:25 -0400

    redid solution and added pseudocode

--- leetcode/romanToInteger/main.js
+++ leetcode/romanToInteger/main.js
@@ -10,6 +10,7 @@ L             50
 C             100
 D             500
 M             1000
+
 For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
 Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
@@ -72,7 +73,7 @@ const romanToInt = s => {
         // since i've accounted for M & C i need to move onto next set by incrementing by one
         i++
       }
-      // if there is no nextInt to compare to, add currentInt to total
+      // if there is no nextInt to compare to, add currentInt to the total
     } else {
       result += currentInt
     }
