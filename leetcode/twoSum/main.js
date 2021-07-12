1aa5fb4e6d5f8957fd620df08a1a28f8aeb8da5c
Sai <snaadira@gmail.com>
2021-07-12 14:43:12 -0400

    fixed an error in the code

--- leetcode/twoSum/main.js
+++ leetcode/twoSum/main.js
@@ -25,7 +25,7 @@ const twoSum = (nums, target) => {
   let map = {}
   for (let i = 0; i < nums.length; i++) {
     if (map[[nums[i]]] >= 0) {
-      return map[nums[i], i]
+      return [map[nums[i], i]]
     }
     map[target - nums[i]] = i
   }
