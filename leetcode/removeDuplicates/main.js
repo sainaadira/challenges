ba52cf8c0ca2cdd66c5b63f3a94b42a7fda4dc19
Sai <snaadira@gmail.com>
2021-07-14 08:02:24 -0400

    completed solution with prep

--- leetcode/removeDuplicates/main.js
+++ leetcode/removeDuplicates/main.js
@@ -24,7 +24,7 @@ const removeDuplicates = nums => {
   let indexCounter = 0
 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== nums[i - 1]) {
+    if (nums[i] !== nums[i + 1]) {
       nums[indexCounter] = nums[i]
       indexCounter++
     }
