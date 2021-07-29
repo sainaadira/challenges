75715a1cc919cf0ea4b4365bfb9db4eda827837f
Sai <snaadira@gmail.com>
2021-07-29 15:21:56 -0400

    completed solution with prep

---
+++ codewars/js/7kyu/wordToBinary/main.js
@@ -0,0 +1,24 @@
+/* https://www.codewars.com/kata/59859f435f5d18ede7000050/train/javascript
+
+Write a function that takes a string and returns an array containing binary numbers equivalent to the ASCII codes of the characters of the string. The binary strings should be eight digits long.
+
+P: string
+R: array => binary from string
+E: Example: 'man' should return [ '01101101', '01100001', '01101110' ]
+P: create an empty array to hold result
+   loop through string input
+   push input string into array using charCodeAt()
+   use toString() method to turn into a string pass in 2 as radix
+   return the arry
+   
+
+
+ */
+
+const wordToBin = str => {
+  let arr = []
+  for (let i = 0; i < str.length; i++) {
+    arr.push(0 + str[i].charCodeAt().toString(2))
+  }
+  return arr
+}
