fab795498aaabab85352a0ce98532ebdf7b4dc0e
Sai <snaadira@gmail.com>
2021-08-06 11:54:23 -0400

    completed solution with prep

---
+++ codewars/js/8kyu/fakeBinary/main.js
@@ -0,0 +1,16 @@
+/*
+https://www.codewars.com/kata/57eae65a4321032ce000002d/train/javascript
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+ -- split string into an array
+ -- iterate through array with map
+ -- conditionally check if numebr is greater or less than 5
+ -- if less: return 0, if greater: return 1
+ -- join back into string
+ */
+
+
+const fakeBin = x => {
+  let nums = x.split('')
+  return nums.map(el => el < 5 ? 0 : 1).join('')
+}
