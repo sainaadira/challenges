ded7ac6244c73043da0f0a254e38dd9a1ad031cc
Sai <snaadira@gmail.com>
2021-08-13 06:52:33 -0400

    completed solution with prep

---
+++ codewars/js/7kyu/sumOfDigits/main.js
@@ -0,0 +1,31 @@
+/* https://www.codewars.com/kata/59cf805aaeb28438fe00001c/train/javascript
+
+It involves implementing a program that sums the digits of a non-negative integer. For example, the sum of 3433 digits is 13.
+
+Digits can be a number or a string, and you should control it is no undefined and return an empty string.
+
+To give you a little more excitement, the program will not only write the result of the sum, but also write all the sums used: 3 + 4 + 3 + 3 = 13.
+
+
+P: number OR string 
+R: total (string)
+E: '3433' => '3 + 4 + 3 + 3' = 13.
+P: check if string is empty, if so return empty string
+   convert digits into string
+   split string into an array
+   call reduce to grab total 
+   create string to add + between each number
+   return sum
+  
+
+*/
+
+const sum = digits => {
+  if (digits === '') return ''
+
+  let sum = String(digits).split('').reduce((a, b) => a + +b, 0)
+  let str = String(digits).split('').join(' + ')
+
+  return `${str} = ${sum}`
+
+}
