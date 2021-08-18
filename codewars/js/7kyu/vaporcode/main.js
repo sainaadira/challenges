8e48e7f76d192e3c2e8eff39f3fc0eb1aaa8ff5b
Sai <snaadira@gmail.com>
2021-08-18 10:22:34 -0400

    completed solution with prep

---
+++ codewars/js/7kyu/vaporcode/main.js
@@ -0,0 +1,28 @@
+/* https://www.codewars.com/kata/5966eeb31b229e44eb00007a/train/javascript
+
+Write a function that converts any sentence into a V A P O R W A V E sentence. a V A P O R W A V E sentence converts all the letters into uppercase, and adds 2 spaces between each letter (or special character) to create this V A P O R W A V E effect.
+
+P: string
+R: S  T  R  I  N  G 
+E: "Lets go to the movies" =>  
+  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
+Pseudocode: 
+ -  change casing toUpperCase()
+ - split string into an array
+ - join array back into a string with two spaces
+ - return string
+ */
+
+
+// this code works but it does not solve for special characters
+const vaporcode = string => {
+  return string.toUpperCase().split('').join('  ')
+}
+
+vaporcode('Let\'s go to the movies')
+
+
+// code that works solves all codewars test cases
+const vaporcode = (string) => {
+  return string.toUpperCase().split(' ').join('').split('').join('  ')
+}
