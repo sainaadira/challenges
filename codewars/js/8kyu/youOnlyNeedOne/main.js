506b6176bcd72582968b41b8732fb18167126d5c
Sai <snaadira@gmail.com>
2021-10-15 10:21:36 -0400

    refactored solution

--- codewars/js/8kyu/youOnlyNeedOne/main.js
+++ codewars/js/8kyu/youOnlyNeedOne/main.js
@@ -4,6 +4,5 @@ Array can contain numbers or strings. X can be either.
 
 Return true if the array contains the value, false if not. */
 
-const check = (a, x) => {
-  return a.includes(x)
-}
+const check = (a, x) => a.includes(x)
+
