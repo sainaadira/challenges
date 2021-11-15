08d74fe805da75ab5e91d202968cf160361bbe30
Sai <snaadira@gmail.com>
2021-11-14 20:29:53 -0500

    refactored solution

--- codewars/js/7kyu/wordToBinary/main.js
+++ codewars/js/7kyu/wordToBinary/main.js
@@ -14,9 +14,9 @@ P: create an empty array to hold result
  */
 
 const wordToBin = str => {
-  let arr = []
+  let result = []
   for (let i = 0; i < str.length; i++) {
-    arr.push(0 + str[i].charCodeAt().toString(2))
+    result.push(0 + str[i].charCodeAt().toString(2))
   }
-  return arr
+  return result
 }
