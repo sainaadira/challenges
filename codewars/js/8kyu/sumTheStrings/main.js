9586263ed65502ec38ee71c91f7f0d840fe67dc8
Sai <snaadira@gmail.com>
2021-12-06 18:33:04 -0500

    refactored solution

--- codewars/js/8kyu/sumTheStrings/main.js
+++ codewars/js/8kyu/sumTheStrings/main.js
@@ -6,14 +6,5 @@ Create a function that takes 2 positive integers in form of a string as an input
   sumStr("34", "5")   // should output "39" */
 
 
-const sumStr = (a, b) => {
-  return `${Number(a) + Number(b)}`
-}
+const sumStr = (a, b) => `${Number(a) + Number(b)}`
 
-// or store the numbers in variables and add them in template literals
-
-const sumStr = (a, b) => {
-  let num1 = Number(a)
-  let num2 = Number(b)
-  return `${num1 + num2}`
-}
