820529462ebfac10de1e37e8e011d26f51101217
Sai <snaadira@gmail.com>
2021-12-07 20:15:15 -0500

    refactored solution

--- codewars/js/8kyu/isPalindrome/main.js
+++ codewars/js/8kyu/isPalindrome/main.js
@@ -3,7 +3,4 @@ https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/javascript
 
  Write a function that checks if a given string (case insensitive) is a palindrome.  */
 
-const isPalindrome = (x) => {
-  x = x.toLowerCase()
-  return x === x.split('').reverse().join('')
-}
+const isPalindrome = (x) => x.toLowerCase() === x.split('').reverse().join('')
