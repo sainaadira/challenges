1125092336e4f971247d3189a4d1be3e1c5f1fc4
Sai <snaadira@gmail.com>
2021-12-12 11:03:32 -0500

    fixed spacing in file

--- codewars/js/8kyu/usdToCny/main.js
+++ codewars/js/8kyu/usdToCny/main.js
@@ -7,8 +7,6 @@ For Example:
 usdcny(15)  => '101.25 Chinese Yuan'
   usdcny(465) => '3138.75 Chinese Yuan'
 The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers should be represented as a string with 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")
-
-
 */
 
 const usdcny = usd => {
