a773bdc4c6bd337a1ceb8368abe923d2211cebb7
Sai <snaadira@gmail.com>
2021-12-15 09:56:50 -0500

    fixed line spacing in solution

--- codewars/js/8kyu/returnTwoHighestInArr/main.js
+++ codewars/js/8kyu/returnTwoHighestInArr/main.js
@@ -5,7 +5,6 @@ In this kata, your job is to return the two distinct highest values in a list. I
 The result should also be ordered from highest to lowest.
 
 Examples:
-
 [4, 10, 10, 9]  =>  [10, 9]
 [1, 1, 1]  =>  [1]
 []  =>  []
