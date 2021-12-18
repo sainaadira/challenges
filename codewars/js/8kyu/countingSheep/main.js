1738a2b2e9e7923bf9894371787f532a77040b86
Sai <snaadira@gmail.com>
2021-12-18 14:53:53 -0500

    fixed spacing in file and solution

--- codewars/js/8kyu/countingSheep/main.js
+++ codewars/js/8kyu/countingSheep/main.js
@@ -2,10 +2,7 @@
 https://www.codewars.com/kata/54edbc7200b811e956000556/train/javascript
 
 Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
-
 */
-
-
 
 const countSheeps = (arrayOfSheep) => {
   let sheepCount = 0
