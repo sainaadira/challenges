431a24038d410c9deddad47a0a93cc60932182db
Sai <snaadira@gmail.com>
2022-01-07 20:36:11 -0500

    fixed spacing in file

--- codewars/js/7kyu/onesAndZeros/main.js
+++ codewars/js/7kyu/onesAndZeros/main.js
@@ -3,8 +3,6 @@ Given an array of ones and zeroes, convert the equivalent binary value to an int
 
 Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
 
-.
-
 param: data working with: array
 returns: number
 examples:
