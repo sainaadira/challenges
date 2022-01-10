5108f0594ba9280f201cb86b271d66acfbabac50
Sai <snaadira@gmail.com>
2022-01-09 21:27:13 -0500

    adding refactored solution

--- codewars/js/7kyu/jsArrayFilter/main.js
+++ codewars/js/7kyu/jsArrayFilter/main.js
@@ -9,8 +9,6 @@ getEvenNumbers([2,4,5,6]) // should == [2,4,6]
 
 data working with: array
 return: array with even numbers
-
 */
-
 
 const getEvenNumbers = numbersArray => numbersArray.filter(num => num % 2 === 0)
