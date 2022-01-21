71ca199f205c81832cb2a4b3f520cb983ddcfabd
Sai <snaadira@gmail.com>
2022-01-21 18:36:26 -0500

    fixed spacing in file

--- codewars/js/8kyu/nameShuffler/main.js
+++ codewars/js/8kyu/nameShuffler/main.js
@@ -4,6 +4,5 @@ https://www.codewars.com/kata/559ac78160f0be07c200005a/train/javascript
 Write a function that returns a string in which firstname is swapped with last name.
 nameShuffler('john McClane'); => "McClane john" */
 
-
 const nameShuffler = str => str.split(' ').reverse().join(' ')
 
