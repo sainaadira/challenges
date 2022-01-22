1c42a8386f2e66c6a1aa47e89c2ac2a5f430a904
Sai <snaadira@gmail.com>
2022-01-22 18:35:03 -0500

    fixed spacing in file

--- codewars/js/8kyu/tipCalculator/main.js
+++ codewars/js/8kyu/tipCalculator/main.js
@@ -3,7 +3,6 @@
 Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service.
 
 You need to consider the following ratings:
-
 Terrible: tip 0%
 Poor: tip 5%
 Good: tip 10%
