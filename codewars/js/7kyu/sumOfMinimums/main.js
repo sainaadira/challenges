2332180dc2b6428c7b9e0d2b65d7b677b81cc3ce
Sai <snaadira@gmail.com>
2022-03-20 16:37:23 -0400

    fix: fix spacing in file

--- codewars/js/7kyu/sumOfMinimums/main.js
+++ codewars/js/7kyu/sumOfMinimums/main.js
@@ -2,8 +2,6 @@
 
 Given a 2D array of size m * n. Your task is to find the sum of minimum value in each row.
 
-For Example:
-
 [
   [1, 2, 3, 4, 5],       // minimum value of row is 1
   [5, 6, 7, 8, 9],       // minimum value of row is 5
