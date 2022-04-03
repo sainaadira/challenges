b2180cd78faf78458212201eb9eec170298800e8
Sai <snaadira@gmail.com>
2022-04-02 22:15:47 -0400

    remove: remove line of code

--- codewars/js/6kyu/gcfOfAnArr/main.js
+++ codewars/js/6kyu/gcfOfAnArr/main.js
@@ -10,10 +10,6 @@ R: +number (gcf of array)
 E: [46, 14, 20, 88]  =>  2
 P: keeping it 100, i havent touched prime #s and gcf since high school so i had to refresh and understand, but now that i'm caught up.. i tried to write out how to solve this problem by using the steps i learned regarding finding the gcd.. only to realize there is just a recursive algorithm (euclidean) out there for figuring it out (nice to know, no need to memorize though) it can be written:
 
-const gcd = (x, y) => (!y) ? x : gcd(y, (x % y))
-
- or
-
    const gcf = (a, b) =>{
     if (b === 0) return a;
     return gcf(b, a % b);
