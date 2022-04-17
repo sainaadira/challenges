b8729e0eceac28ca09161a1fa3953608c57cdd76
Sai <snaadira@gmail.com>
2022-04-17 19:57:15 -0400

    fix: fix file formatting

--- codewars/js/8kyu/fundamentals:return/main.js
+++ codewars/js/8kyu/fundamentals:return/main.js
@@ -6,17 +6,11 @@ Make multiple functions that will return the sum, difference, modulus, product, 
 Please use the following function names:
 
 addition = add
-
 multiply = multiply
-
 division = divide (both integer and float divisions are accepted)
-
 modulus = mod
-
 exponential = exponent
-
 subtraction = subt
-
 Note: All math operations will be: a (operation) b
 
 a (operation) b*
